ae679c40cc5990b37f0413b562fdcffa59e23d01
ZRichV <597497946@qq.com>
2020-04-11 20:46:16 +0800

    new project

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login_container">
+        loigin
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    height: 100%;
+    background-color: #2C71B8;
+}
+</style>
