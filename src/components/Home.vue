d5ef0a0d6eae94e536cd9bc359851e365ffe5a7b
ZRichV <597497946@qq.com>
2020-04-14 22:48:54 +0800

    update_home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,147 @@
 <template>
-    <div>
-        Home
-    </div>
+  <el-container>
+    <!-- head -->
+    <el-header>
+      <div>
+        <span>午夜心碎俱乐部</span>
+      </div>
+      <el-button round @click="logout">Lover Boy</el-button>
+    </el-header>
+    <!-- body -->
+    <el-container>
+      <!-- leftSideMenus -->
+      <el-aside :width="isCollapse ? '70px' : '250px'">
+        <div class="toggle-button" @click="doCollapse">Toosie Slide</div>
+        <!-- :unique-opened="true"-> 是否只保持一个子菜单的展开 -->
+        <!-- :collapse-transition="false" -> 关闭动画 -->
+        <!-- router -> 导航开启路由模式 -->
+        <!-- :default-active="activePath" -> 当前激活菜单的 index -->
+        <el-menu
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="true"
+          router
+          :default-active="activePath"
+          background-color="#CDCDCD"
+          text-color="#666666"
+          active-text-color="#FF9655"
+        >
+          <!-- lv1 menu-->
+          <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+            <!-- lv1 template -->
+            <template slot="title">
+              <i class="el-icon-no-smoking"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- lv2 menu -->
+            <!-- 导航开启路由模式：将index值作为导航路由 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item,children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <!-- lv2 template -->
+              <template slot="title">
+                <i class="el-icon-moon"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- main content -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      // leftMenuDate
+      menuList: [],
+      // default collapse
+      isCollapse: false,
+      // path of active
+      activePath: ''
+    };
+  },
+  created() {
+    this.getMenuList(),
+      (this.activePath = window.sessionStorage.getItem("activePath"));
+  },
+  methods: {
+    logout() {
+      // clear token
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    // get menu
+    async getMenuList() {
+      const { date: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data;
+    },
+    // menu collapse
+    doCollapse() {
+      this.isCollapse = !this.isCollapse;
+    },
+    // save activePath
+    saveNaveState(activePath) {
+      window.sessionStorage.setItem("activePath", activePath);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #F2F6FC;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 90px;
+  align-items: center;
+  color: #000000;
+  font-size: 30px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      height: 40px;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #F2F6FC;
+
+  .el-menu {
+    border: 5px;
+  }
+}
+.el-main {
+  background-color: #EBEEF5;
+}
+.iconfont{
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #EBEEF5;
+  font-size: 17px;
+  line-height: 24px;
+  color: #000000;
+  text-align: center;
+  letter-spacing: 0.2em;
+  // 鼠标放上去变成小手
+  cursor: pointer;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -23,13 +23,13 @@ const router = new VueRouter({
   routes
 })
 
-// 挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
-// router.beforeEach((from, to, next) => {
-//   if(to.path === '/login') return next()
-//   // 获取token
-//   const tokenStr = window.sessionStorage.getItem('token')
-//   if(!tokenStr) return next('/login')
-//   next()
-// })
+//挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+  if(to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
+})
 
 export default router
